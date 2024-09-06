57ff9f92f9e06164783ba0c1f958c61c1935f7c8
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-09-06 08:31:27 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,5 +1,7 @@
-
+const nomes = ["Alana", "Ana", "Gian", "Sabrina"];
 export function aleatorio (Lista) {
     const posicao = Math.floor(Math.random()* Lista.length);
     return Lista[posicao]
 }
+
+const nome = aleatorio(nomes);
