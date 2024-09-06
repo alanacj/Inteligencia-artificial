94a88fd15c6a8f5338ddf222ef43945c9873082c
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-09-06 11:14:47 -0300

    Update scripit.js

--- js/scripit.js
+++ js/scripit.js
@@ -1,18 +1,27 @@
-import { aleatorio, nome} from "./aleatorio.js";
-import { perguntas } from "./perguntas";
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultados = document.querySelector(".caixa-Resultados");
+const caixaResultados = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente = document.querySelector("novamente-btn");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 const botaoIniciar = document.querySelector(".iniciar-btn");
-const telaInicial = document.querySelector("")
+const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal= "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -34,39 +43,45 @@ function mostraAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
-     const afirmacoes = aleatorio(opcaoSelecionada.afirmacoes);
-     historiaFinal += afirmacoes + " ";
-     if(opcaoSelecionada.proxima !== undefined){
-        atual = opcaoSeleciona.proxima;
-     }else{
-        mostraResultado();
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + " ";
+    if(opcaoSelecionada.proxima !== undefined) {
+        atual = opcaoSelecionada.proxima;
+    } else {
+        mostraResultado ();
         return;
-     }
-     mostraPergunta();
-
-}
-
-function mostraResultado(){
-    caixaPerguntas.textContent = 'Em 2049, ${nome}';
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "A educação terá evoluído significativamente, moldada por avanços tecnológicos, mudanças sociais e novas abordagens pedagógicas. Este futuro educacional será profundamente integrado à tecnologia, mas também focado no desenvolvimento holístico dos estudantes. Sua escolha fará toda diferença, você pode ou não querer evoluir junto, mas, não podemos privar nosso futuro disso.";
-    caixaResultados.classList.add("mostrar");
-    botaoJogarNovamente.addEventListener("click", botaoJogarNovamente);
-}
-
-functionjogaNovamente();{
-    atual = 0;
-    historiaFinal = "";
-    caixaResultados.classList.remove("mostrar");
+    }
     mostraPergunta();
 }
 
-function substituiNome(){
-    for(const pergunta of perguntas){
-    pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
-        }
-    }    
+// function mostraResultado(){
+//     caixaPerguntas.textContent = 'Em 2049, ${nome}';
+//     textoResultado.textContent = historiaFinal;
+//     caixaAlternativas.textContent = "A educação terá evoluído significativamente, moldada por avanços tecnológicos, mudanças sociais e novas abordagens pedagógicas. Este futuro educacional será profundamente integrado à tecnologia, mas também focado no desenvolvimento holístico dos estudantes. Sua escolha fará toda diferença, você pode ou não querer evoluir junto, mas, não podemos privar nosso futuro disso.";
+//     caixaResultados.classList.add("mostrar");
+//     botaoJogarNovamente.addEventListener("click", jogaNovamente);
+// }
+
+function mostraResultado() {
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+    caixaResultados.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
+}
+
+function jogaNovamente () {
+    atual = 0;
+    historiaFinal = "";
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta ();
+}
+
+function substituiNome() {
+    for(const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
 
 substituiNome();
-mostraPergunta();
