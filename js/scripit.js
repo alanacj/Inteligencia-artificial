69ca485ea94ca51facecee7473c855aeb77d7dd8
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-09-06 08:32:33 -0300

    Update scripit.js

--- js/scripit.js
+++ js/scripit.js
@@ -1,4 +1,4 @@
-import { aleatorio} from "./aleatorio";
+import { aleatorio, nome} from "./aleatorio.js";
 import { perguntas } from "./perguntas";
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
@@ -7,6 +7,8 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-Resultados");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector("novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector("")
 
 let atual = 0;
 let perguntaAtual;
@@ -35,13 +37,18 @@ function mostraAlternativas(){
 function respostaSelecionada(opcaoSelecionada){
      const afirmacoes = aleatorio(opcaoSelecionada.afirmacoes);
      historiaFinal += afirmacoes + " ";
-     atual++;
+     if(opcaoSelecionada.proxima !== undefined){
+        atual = opcaoSeleciona.proxima;
+     }else{
+        mostraResultado();
+        return;
+     }
      mostraPergunta();
 
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = 'Em 2049, ${nome}';
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "A educação terá evoluído significativamente, moldada por avanços tecnológicos, mudanças sociais e novas abordagens pedagógicas. Este futuro educacional será profundamente integrado à tecnologia, mas também focado no desenvolvimento holístico dos estudantes. Sua escolha fará toda diferença, você pode ou não querer evoluir junto, mas, não podemos privar nosso futuro disso.";
     caixaResultados.classList.add("mostrar");
@@ -55,4 +62,11 @@ functionjogaNovamente();{
     mostraPergunta();
 }
 
+function substituiNome(){
+    for(const pergunta of perguntas){
+    pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+        }
+    }    
+
+substituiNome();
 mostraPergunta();
